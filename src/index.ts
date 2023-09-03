aae8a77fbf834ae3b7e5848416afc5aa1a7da209
Andrew Clemons <andrew.clemons@gmail.com>
2023-09-03 22:38:47 +0900

    Scaffold for commands to build.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import { Probot } from 'probot';
 
 export = (app: Probot) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({
-      body: 'Thanks for opening this issue!',
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on('issue_comment.created', async (context) => {
+    context.log.info('Processing comment');
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    if (context.isBot) {
+      context.log.info('Ignoring bots (and self).');
+      return;
+    }
+
+    const { payload } = context;
+
+    if (!payload.issue.pull_request) {
+      context.log.info('Payload is not a pull request comment.');
+      return;
+    }
+
+    const { owner, repo } = context.repo();
+
+    // TODO for now we only allow the testing repo.
+    if (`${owner}/${repo}` !== 'aclemons/-slackbuilds-test') {
+      context.log.info('Comment is not test repo.');
+      return;
+    }
+
+    if (payload.comment.body.trim().startsWith('@sbo-bot: build')) {
+      await context.octokit.reactions.createForIssueComment({owner: context.repo().owner, repo: context.repo().repo, comment_id: payload.comment.id, content: '+1'});
+    }
+  });
 };
