dff9397c5cf7d60c0dae100f423174c3d47be2e9
Andrew Clemons <andrew.clemons@gmail.com>
2023-09-09 23:26:56 +0900

    First cut of building based off admin comments.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { Probot } from 'probot';
+import axios from 'axios';
+
+const allowedCommentors = (process.env.GITHUB_ADMINS || '').split(",");
 
 export = (app: Probot) => {
   app.on('issue_comment.created', async (context) => {
@@ -24,8 +27,64 @@ export = (app: Probot) => {
       return;
     }
 
-    if (payload.comment.body.trim().startsWith('@sbo-bot: build')) {
-      await context.octokit.reactions.createForIssueComment({owner: context.repo().owner, repo: context.repo().repo, comment_id: payload.comment.id, content: '+1'});
+    if (!allowedCommentors.includes(payload.comment.user.login)) {
+      context.log.info('Comment was not made by an admin.');
+      return;
+    }
+
+    if (!process.env.JENKINS_WEBHOOK || !process.env.JENKINS_WEBHOOK_SECRET) {
+      context.log.info('Jenkins webhook vars not configured');
+      return;
+    }
+
+    const comment = payload.comment.body.trim();
+    const matches = comment.match("^(@sbo-bot: build) ((amd64|x86_64|arm|i586) )?([a-zA-z]+\\/[a-zA-Z0-9\\+\\-]+)$");
+
+    if (!matches) {
+      context.log.info('Comment not a build request.');
+      return;
+    }
+
+    const build_arch = matches[3];
+    const build_package = matches[4];
+
+    if (build_arch) {
+      context.log.info(`Triggering build of package ${build_package} with requested arch: ${build_arch}.`);
+      const { data, status } = await axios.post(process.env.JENKINS_WEBHOOK, { build_arch: build_arch, 'gh_pr': payload.issue.number, build_package: build_package }, { headers: { 'Content-Type': 'application/json', token: process.env.JENKINS_WEBHOOK_SECRET } })
+
+      context.log.info(`Received jenkins response: ${JSON.stringify(data)}`);
+
+      if (status === 200 && data['triggered']) {
+        context.log.info("Build was successfully scheduled.");
+        await context.octokit.reactions.createForIssueComment({ owner: context.repo().owner, repo: context.repo().repo, comment_id: payload.comment.id, content: '+1' });
+        context.log.info("Confirmed build triggering by thumbs-upping comment.");
+      } else {
+        context.log.info("No job triggered.");
+      }
+    } else {
+      context.log.info(`Triggering build of package ${build_package} for both i586 and x86_64.`);
+
+      let { data, status } = await axios.post(process.env.JENKINS_WEBHOOK, { build_arch: 'i586', 'gh_pr': payload.issue.number, build_package: build_package }, { headers: { 'Content-Type': 'application/json', token: process.env.JENKINS_WEBHOOK_SECRET } })
+
+      context.log.info(`Received jenkins response for i586 trigger: ${JSON.stringify(data)}`);
+
+      if (status === 200 && data["jobs"]["slackbuilds.org-pr-check-build-package"]['triggered']) {
+        context.log.info("Build (i586) was successfully scheduled.");
+
+        let { data, status } = await axios.post(process.env.JENKINS_WEBHOOK, { build_arch: 'amd64', 'gh_pr': payload.issue.number, build_package: build_package }, { headers: { 'Content-Type': 'application/json', token: process.env.JENKINS_WEBHOOK_SECRET } })
+
+        context.log.info(`Received jenkins response for x86_64 trigger: ${JSON.stringify(data)}`);
+
+        if (status === 200 && data["jobs"]["slackbuilds.org-pr-check-build-package"]['triggered']) {
+          context.log.info("Build (x86_64) was successfully scheduled.");
+          await context.octokit.reactions.createForIssueComment({ owner: context.repo().owner, repo: context.repo().repo, comment_id: payload.comment.id, content: '+1' });
+          context.log.info("Confirmed build triggerings by thumbs-upping comment.");
+        } else {
+          context.log.info("No second job triggered.");
+        }
+      } else {
+        context.log.info("No job triggered.");
+      }
     }
   });
 };
