b9bd733975ef210e22c0fb8c5025c74b1ccc3a02
Andrew Clemons <andrew.clemons@gmail.com>
2023-09-10 19:28:00 +0900

    Send the repo along with the build request.

--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,6 @@ export = (app: Probot) => {
       return;
     }
 
-    const { owner, repo } = context.repo();
-
-    // TODO for now we only allow the testing repo.
-    if (`${owner}/${repo}` !== 'aclemons/-slackbuilds-test') {
-      context.log.info('Comment is not test repo.');
-      return;
-    }
-
     if (!allowedCommentors.includes(payload.comment.user.login)) {
       context.log.info('Comment was not made by an admin.');
       return;
@@ -50,11 +42,14 @@ export = (app: Probot) => {
 
     if (build_arch) {
       context.log.info(`Triggering build of package ${build_package} with requested arch: ${build_arch}.`);
-      const { data, status } = await axios.post(process.env.JENKINS_WEBHOOK, { build_arch: build_arch, 'gh_pr': payload.issue.number, build_package: build_package }, { headers: { 'Content-Type': 'application/json', token: process.env.JENKINS_WEBHOOK_SECRET } })
+      const { data, status } = await axios.post(
+        process.env.JENKINS_WEBHOOK, { build_arch: build_arch, 'gh_pr': payload.issue.number, build_package: build_package, repo: `${context.repo().owner}/${context.repo().repo}` },
+        { headers: { 'Content-Type': 'application/json', token: process.env.JENKINS_WEBHOOK_SECRET } }
+      );
 
       context.log.info(`Received jenkins response: ${JSON.stringify(data)}`);
 
-      if (status === 200 && data['triggered']) {
+      if (status === 200 && data["jobs"]["slackbuilds.org-pr-check-build-package"]['triggered']) {
         context.log.info("Build was successfully scheduled.");
         await context.octokit.reactions.createForIssueComment({ owner: context.repo().owner, repo: context.repo().repo, comment_id: payload.comment.id, content: '+1' });
         context.log.info("Confirmed build triggering by thumbs-upping comment.");
@@ -64,14 +59,20 @@ export = (app: Probot) => {
     } else {
       context.log.info(`Triggering build of package ${build_package} for both i586 and x86_64.`);
 
-      let { data, status } = await axios.post(process.env.JENKINS_WEBHOOK, { build_arch: 'i586', 'gh_pr': payload.issue.number, build_package: build_package }, { headers: { 'Content-Type': 'application/json', token: process.env.JENKINS_WEBHOOK_SECRET } })
+      let { data, status } = await axios.post(
+        process.env.JENKINS_WEBHOOK, { build_arch: 'i586', 'gh_pr': payload.issue.number, build_package: build_package, repo: `${context.repo().owner}/${context.repo().repo}` },
+        { headers: { 'Content-Type': 'application/json', token: process.env.JENKINS_WEBHOOK_SECRET } }
+      );
 
       context.log.info(`Received jenkins response for i586 trigger: ${JSON.stringify(data)}`);
 
       if (status === 200 && data["jobs"]["slackbuilds.org-pr-check-build-package"]['triggered']) {
         context.log.info("Build (i586) was successfully scheduled.");
 
-        let { data, status } = await axios.post(process.env.JENKINS_WEBHOOK, { build_arch: 'amd64', 'gh_pr': payload.issue.number, build_package: build_package }, { headers: { 'Content-Type': 'application/json', token: process.env.JENKINS_WEBHOOK_SECRET } })
+        let { data, status } = await axios.post(
+          process.env.JENKINS_WEBHOOK, { build_arch: 'x86_64', 'gh_pr': payload.issue.number, build_package: build_package, repo: `${context.repo().owner}/${context.repo().repo}` },
+          { headers: { 'Content-Type': 'application/json', token: process.env.JENKINS_WEBHOOK_SECRET } }
+        );
 
         context.log.info(`Received jenkins response for x86_64 trigger: ${JSON.stringify(data)}`);
 
