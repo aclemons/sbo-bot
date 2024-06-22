76a6eabd730cfcd05ed8c784ceb161bd7609b6c1
Andrew Clemons <andrew.clemons@gmail.com>
2024-06-22 18:07:10 +0900

    Run tests on PRs.
    For now, we only have e2e tests, but in future, this will also run
    pytest and jest tests for each lambda.

--- e2e-tests/jest.config.ts
+++ e2e-tests/jest.config.ts
@@ -3,6 +3,7 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testTimeout: 15000,
 };
 
 export default jestConfig;
